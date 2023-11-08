204d33f42b6c83376dce63d18a274ecdfc47cbfc
bsaranga <bsaranga@gmail.com>
2023-11-08 22:18:15 +0530

    0mq stuff

---
+++ ZMQClient/Program.cs
@@ -0,0 +1,31 @@
+﻿using NetMQ;
+using NetMQ.Sockets;
+
+namespace ZMQClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var client = new RequestSocket())
+            {
+                client.Connect("tcp://localhost:5555");
+                while (true)
+                {
+                    Console.Write("Enter message: ");
+                    string message = Console.ReadLine() ?? "<empty_message>";
+                    if (message == "exit" || message == "x")
+                        break;
+                    else
+                    {
+                        client.SendFrame(message);
+                        var response = client.ReceiveFrameString();
+                        Console.WriteLine($"Received response from server:\n{response}");
+                    }
+                }
+            }
+
+            Console.WriteLine("Client ended");
+        }
+    }
+}
---
+++ ZMQServer/Program.cs
@@ -0,0 +1,23 @@
+﻿using NetMQ;
+using NetMQ.Sockets;
+
+namespace ZMQServer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var server = new ResponseSocket())
+            {
+                server.Bind("tcp://*:5555");
+                Console.WriteLine("Listening on port 5555...");
+                while (true)
+                {
+                    var message = server.ReceiveFrameString();
+                    Console.WriteLine($"Received: {message}");
+                    server.SendFrame($"Ack: {message}");
+                }
+            }
+        }
+    }
+}
