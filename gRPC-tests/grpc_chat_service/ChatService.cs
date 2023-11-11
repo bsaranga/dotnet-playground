43f03535f796116c554aca3f0f111dabf264a190
bsaranga <bsaranga@gmail.com>
2023-11-11 13:36:26 +0530

    grpc tests

---
+++ gRPC-tests/grpc_chat_service/ChatService.cs
@@ -0,0 +1,19 @@
+﻿using Grpc.Core;
+
+namespace grpc_chat_service
+{
+    class ChatService : Chat.ChatBase
+    {
+        public override async Task SendMessage(IAsyncStreamReader<MessageRequest> requestStream, IServerStreamWriter<MessageResponse> responseStream, ServerCallContext context)
+        {
+            await foreach (var request in requestStream.ReadAllAsync())
+            {
+                // Process the incoming message
+                Console.WriteLine($"Received message: {request.Content}");
+
+                // Send a response
+                await responseStream.WriteAsync(new MessageResponse { Content = $"Server received: {request.Content}" });
+            }
+        }
+    }
+}
---
+++ gRPC-tests/grpc_chat_service/Program.cs
@@ -0,0 +1,25 @@
+﻿using Grpc.Core;
+
+namespace grpc_chat_service
+{
+    internal class Program
+    {
+        const int Port = 50051;
+
+        static void Main(string[] args)
+        {
+            Server server = new Server
+            {
+                Services = { Chat.BindService(new ChatService()) },
+                Ports = { new ServerPort("localhost", Port, ServerCredentials.Insecure) }
+            };
+            server.Start();
+
+            Console.WriteLine($"Server listening on port {Port}");
+            Console.WriteLine("Press any key to stop the server...");
+            Console.ReadKey();
+
+            server.ShutdownAsync().Wait();
+        }
+    }
+}
