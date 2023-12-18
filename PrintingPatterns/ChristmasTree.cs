eb23c28e55e4561b6efa7727a38d9388bc725689
bsaranga <bsaranga@gmail.com>
2023-12-19 00:42:07 +0530

    wip

---
+++ PrintingPatterns/ChristmasTree.cs
@@ -0,0 +1,39 @@
+﻿namespace PrintingPatterns
+{
+    /*
+    ____*____
+    ___***___
+    __*****__
+    _*******_
+    *********
+
+    nth_odd = 2(n) - 1, where n > 0
+    nRows = treeHeight
+    nCols = 2(nRows) - 1
+
+            1
+           2 2
+          3 3 3
+         4 4 4 4
+        5 5 5 5 5 
+     */
+    internal class ChristmasTree : IPatternPrinter
+    {
+        public void Print()
+        {
+            int treeHeight = 5;
+            int nCols = (2 * treeHeight - 1);
+            for (int i = 1; i <= treeHeight; i++)
+            {
+                for (int j = 1; j <= nCols; j++)
+                {
+                    int numStars = 2 * i - 1;
+                    int numSpace = nCols - numStars;
+                    Console.Write("_");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ PrintingPatterns/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace PrintingPatterns
+{
+    public interface IPatternPrinter
+    {
+        void Print();
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IPatternPrinter christmasTree = new ChristmasTree();
+            christmasTree.Print();
+        }
+    }
+}
