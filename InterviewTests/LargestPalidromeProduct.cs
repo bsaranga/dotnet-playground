011cbdae4d1efa2813708df99adb95677f5afa15
bsaranga <bsaranga@gmail.com>
2023-12-19 16:03:01 +0530

    largest palindrome product

---
+++ InterviewTests/LargestPalidromeProduct.cs
@@ -0,0 +1,24 @@
+﻿using InterviewTests;
+
+namespace InterviewPracticeCode
+{
+    public class LargestPalidromeProduct
+    {
+        public static double GetLargestPalindromeProduct(int numDigits)
+        {
+            double currentMax = 0;
+            double startVal = Math.Pow(10, numDigits - 1);
+            double endVal = Math.Pow(10, numDigits);
+
+            for (double i = startVal; i < endVal; i++)
+                for (double j = startVal; j < endVal; j++)
+                {
+                    double prod = i * j;
+                    if (prod > currentMax && Palindrome.IsPalidrome(prod.ToString()))
+                        currentMax = prod;
+                }
+
+            return currentMax;
+        }
+    }
+}
--- InterviewTests/Program.cs
+++ InterviewTests/Program.cs
@@ -1,13 +1,12 @@
-﻿namespace InterviewTests
+﻿using InterviewPracticeCode;
+
+namespace InterviewTests
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            var point = new Point(5, 6);
-            point.ShowPoint();
-
-            Console.ReadLine();
+            
         }
     }
 }
